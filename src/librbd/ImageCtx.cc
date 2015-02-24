68afed8435f2afa67ec7673a54d27ae5b1e59ac1
Josh Durgin <jdurgin@redhat.com>
2015-02-23 19:49:12 -0800

    librbd: use snap_lock to protect ImageCtx->flags
    This is another step towards eliminating md_lock from the writeback
    path. Almost all the places that use ImageCtx->flags already use
    snap_lock, so there's no need to create a new lock. For the others,
    add a helper, test_flags() that acquires the lock, similar to
    test_features().
    
    This also makes sure we look up flags of the snapshot we're operating
    on, instead of those for head.
    
    Signed-off-by: Josh Durgin <jdurgin@redhat.com>

--- src/librbd/ImageCtx.cc
+++ src/librbd/ImageCtx.cc
@@ -475,6 +475,7 @@ namespace librbd {
 
   int ImageCtx::get_flags(librados::snap_t _snap_id, uint64_t *_flags) const
   {
+    assert(snap_lock.is_locked());
     if (_snap_id == CEPH_NOSNAP) {
       *_flags = flags;
       return 0;
@@ -485,6 +486,14 @@ namespace librbd {
       return 0;
     }
     return -ENOENT;
+  }
+
+  bool ImageCtx::test_flags(uint64_t test_flags) const
+  {
+    RWLock::RLocker l(snap_lock);
+    uint64_t snap_flags;
+    get_flags(snap_id, &snap_flags);
+    return ((snap_flags & test_flags) == test_flags);
   }
 
   const parent_info* ImageCtx::get_parent_info(snap_t in_snap_id) const
